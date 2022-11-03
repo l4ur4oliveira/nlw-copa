4c1b3e6bc4629f1e23f6006a31e651f54c6c9a16
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-02 23:14:09 -0300

    Add a custom theme and config initial components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { NativeBaseProvider, Center, Text } from 'native-base';
+import { THEME } from './src/styles/theme';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="green.300">
-        <Text color="black" fontSize={24}>Iiiiirra</Text>
-        <StatusBar style="auto" />
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
