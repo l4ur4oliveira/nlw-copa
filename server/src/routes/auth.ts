1f584b65e013649c15efe1cb3e1128c14b49bbf2
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-05 20:48:32 -0300

    Add sign in/sign up with Google oauth

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,58 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
+import axios from "axios"
 import { prisma } from "../lib/prisma"
 
 export async function authRoutes(fastify: FastifyInstance) {
-  
+  fastify.post('/users', async (request) => {
+    const createUserBody = z.object({
+      access_token: z.string(),
+    })
+
+    const { access_token } = createUserBody.parse(request.body)
+
+    const userResponse = await axios.get('https://www.googleapis.com/oauth2/v2/userinfo', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      }
+    })
+
+    const userData = await userResponse.data
+
+    const userInfoSchema = z.object({
+      id: z.string(),
+      email: z.string().email(),
+      name: z.string(),
+      picture: z.string().url(),
+    })
+
+    const userInfo = userInfoSchema.parse(userData)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        googleId: userInfo.id,
+      }
+    })
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.id,
+          name: userInfo.name,
+          email: userInfo.email,
+          avatarUrl: userInfo.picture,
+        }
+      })
+    }
+
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+    }, {
+      sub: user.id,
+      expiresIn: '3 days',
+    })
+
+    return { token }
+  })
 }
