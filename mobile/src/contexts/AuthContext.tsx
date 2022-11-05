9cb66a90306a9210c0b4de420fd15b4b06a21a87
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-05 15:40:39 -0300

    Add Google Authentication

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as AuthSession from "expo-auth-session";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -11,23 +16,52 @@ interface AuthContextProviderProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [ user, setUser] = useState<UserProps>({} as UserProps);
+  const [ isUserLoading, setIsUserLoading] = useState(false);
+  
+  const [request, response, promptAsync ] = Google.useAuthRequest({
+    clientId: 'your-google-client-id',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  });
+
   async function signIn() {
-    console.log('Vamos logar!');
+    try {
+      setIsUserLoading(true);
+
+      await promptAsync();
+
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log('TOKEN ===>', access_token);
+    
+  }
+
+  useEffect(() => {
+    if(response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider value={{
       signIn,
-      user: {
-        name: 'Jane Doe',
-        avatarUrl: 'http://someurl.com/avatar.png',
-      }
+      isUserLoading,
+      user,
     }}>
       {children}
     </AuthContext.Provider>
