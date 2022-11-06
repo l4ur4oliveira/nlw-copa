2c1a0187bbc0201644e5500b4fbee14539b59c95
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-06 01:30:06 -0300

    Add server connection to mobile app and set auth token

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,7 +1,8 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
-import * as AuthSession from "expo-auth-session";
-import * as Google from "expo-auth-session/providers/google";
-import * as WebBrowser from "expo-web-browser";
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import * as AuthSession from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import { api } from '../services/api';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -47,7 +48,20 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   }
 
   async function signInWithGoogle(access_token: string) {
-    console.log('TOKEN ===>', access_token);
+    try {
+      setIsUserLoading(true);
+      
+      const tokenResponse = await api.post('/users', { access_token });
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`;
+
+      const userInfoResponse = await api.get('/me');
+      setUser(userInfoResponse.data.user);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
     
   }
 
