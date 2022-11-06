43d5c3e98d4362ea28e819a24cd3098e569b645d
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-05 22:21:32 -0300

    Add route to get poll matches

--- server/src/routes/match.ts
+++ server/src/routes/match.ts
@@ -1,6 +1,42 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function matchRoutes(fastify: FastifyInstance) {
-  
+  fastify.get('/polls/:id/matches', {
+    onRequest: [authenticate]
+  }, async (request) => {
+    const getPollParams = z.object({
+      id: z.string(),
+    })
+
+    const { id } = getPollParams.parse(request.params)
+
+    const matches = await prisma.match.findMany({
+      orderBy: {
+        date: 'desc'
+      },
+      include: {
+        guesses: {
+          where: {
+            participant: {
+              userId: request.user.sub,
+              pollId: id,
+            }
+          }
+        }
+      }
+    })
+
+    return {
+      matches: matches.map(match => {
+        return {
+          ...match,
+          guess: match.guesses.length > 0 ? match.guesses[0] : null,
+          guesses: undefined,
+        }
+      })
+    }
+  })
 }
