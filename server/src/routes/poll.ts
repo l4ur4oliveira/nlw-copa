63250f9b015f3fbce0ef9ecbad411301315684ce
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-05 21:14:50 -0300

    Create poll with logged user

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import ShortUniqueId from "short-unique-id"
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function pollRoutes(fastify: FastifyInstance) {
   fastify.get('/polls/count', async () => {
@@ -20,12 +21,30 @@ export async function pollRoutes(fastify: FastifyInstance) {
     const generate = new ShortUniqueId({ length: 6 })
     const code = String(generate()).toUpperCase()
 
-    await prisma.poll.create({
-      data: {
-        title,
-        code
-      }
-    })
+    try {
+      await authenticate(request)
+
+      await prisma.poll.create({
+        data: {
+          title,
+          code,
+          ownerId: request.user.sub,
+
+          participants: {
+            create: {
+              userId: request.user.sub
+            }
+          }
+        }
+      })
+    } catch {
+      await prisma.poll.create({
+        data: {
+          title,
+          code
+        }
+      })
+    }
 
     return reply.status(201).send({ code })
   })
