f888bc8de4305ca8654d364cd7480fd6a96383db
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-09 20:30:43 -0300

    Add detail switch to the screen

--- mobile/src/screens/Details.tsx
+++ mobile/src/screens/Details.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useToast, VStack } from 'native-base';
+import { HStack, useToast, VStack } from 'native-base';
 import { useRoute } from '@react-navigation/native';
 
 import { api } from '../services/api';
@@ -9,6 +9,7 @@ import { Header } from '../components/Header';
 import { Loading } from '../components/Loading';
 import { PollCardProps } from '../components/PollCard';
 import { PollHeader } from '../components/PollHeader';
+import { Option } from '../components/Option';
 
 interface RouteParams {
   id: string;
@@ -17,6 +18,7 @@ interface RouteParams {
 export function Details() {
   const [isLoading, setIsLoading] = useState(false);
   const [pollDetails, setPollDetails] = useState<PollCardProps>({} as PollCardProps);
+  const [optionSelected, setOptionSelected] = useState<'guesses' | 'ranking'>('guesses');
 
   const route = useRoute();
   const toast = useToast();
@@ -59,9 +61,22 @@ export function Details() {
       <Header title={pollDetails.title} showBackButton showShareButton />
 
       {
-        pollDetails._count?.participants > 0 ?
+        pollDetails._count?.participants < 0 ?
         <VStack px={5} flex={1}>
           <PollHeader data={pollDetails} />
+
+          <HStack bgColor="gray.800" p={1} rounded="sm" mb={5}>
+            <Option
+              title="Seus palpites"
+              isSelected={optionSelected === 'guesses'}
+              onPress={() => setOptionSelected('guesses')}
+              />
+            <Option
+              title="Ranking do grupo"
+              isSelected={optionSelected === 'ranking'}
+              onPress={() => setOptionSelected('ranking')}
+            />
+          </HStack>
         </VStack>
 
         : <EmptyMyPollList code={pollDetails.code} />
